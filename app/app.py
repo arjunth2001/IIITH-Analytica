b9ceb75034010e60951b444098624f6d6193d8e1
T.H.Arjun <arjun.thekoot@research.iiit.ac.in>
2021-10-26 17:32:43 +0530

    Added all the plots to app

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-PAGES = ["FTC Paragraph Extractor", "Summarizer",
+PAGES = ["Home", "Jurassic-1 Chat Bot", "Longitudinal Analysis and GDPR", "Comparing Forbes 500 vs Indian Startups", "FTC Paragraph Extractor", "Summarizer",
          "Question and Answering", "Explainable AI"]
 PAGE_CONFIG = {'page_title': 'Online Privacy Project', 'layout': "wide"}
 st.set_page_config(**PAGE_CONFIG)
@@ -9,7 +9,19 @@ st.set_page_config(**PAGE_CONFIG)
 def main():
     st.sidebar.title("Menu")
     selection = st.sidebar.radio("Go to", PAGES)
-    if selection == "FTC Paragraph Extractor":
+    if selection == "Home":
+        from home import home
+        home()
+    elif selection == "Jurassic-1 Chat Bot":
+        from jurassic import jurassic
+        jurassic()
+    elif selection == "Longitudinal Analysis and GDPR":
+        from l_analysis import analysis
+        analysis()
+    elif selection == "Comparing Forbes 500 vs Indian Startups":
+        from analysis import analysis
+        analysis()
+    elif selection == "FTC Paragraph Extractor":
         from ftc import ftc
         ftc()
     elif selection == "Summarizer":
